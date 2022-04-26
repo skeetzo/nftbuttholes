c868d257986b3a87c8970ba1d15f01e936ddbda8
Skeetzo <WebmasterSkeetzo@gmail.com>
2022-04-25 21:43:50 -0700

    saved

--- buttholes.js
+++ buttholes.js
@@ -1,8 +1,9 @@
-// https://www.quicknode.com/guides/web3-sdks/how-to-integrate-ipfs-with-ethereum
 
 var ethers = require('ethers');
 var url = "127.0.0.1";
 var provider = new ethers.providers.JsonRpcProvider(url);
+
+////////////////////////////////////////////////////////////////////////////////////
 
 // TODO
 // load abi and address from a file
@@ -11,20 +12,25 @@ var abi = [];
 
 var contract = new ethers.Contract(address, abi, provider);
 
+////////////////////////////////////////////////////////////////////////////////////
 
+// TODO
 // 18+ confirm
 //- allow user to click other buttons
 
+// TODO
 // mint butthole
 //- pay for / add butthole to wallet
-let tokenId = 0;
+// let tokenId = 0;
 
-// view buttholes
+// TODO
+// view uncensored butthole images
 //- get each token owned by wallet
-// function to display all tokens on an html page
+//- add function to display all tokens on an html page
 
-contract.tokenURI(tokenId).then((result) =>{
-  document.getElementById("mint").onclick = function () {
-		location.href = "https://ipfs.io/ipfs/"+result;
-  };
-});
+//- adjust below to be useful for viewing
+// contract.tokenURI(tokenId).then((result) =>{
+//   document.getElementById("mint").onclick = function () {
+// 		location.href = "https://ipfs.io/ipfs/"+result;
+//   };
+// });
--- contracts/Buttholes.sol
+++ contracts/Buttholes.sol
@@ -5,6 +5,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
+//
+import "./PaymentSplitPusher.sol";
+
 
 /**
  * @title Buttholes
@@ -14,7 +17,13 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
 contract Buttholes is Ownable, ERC721URIStorage, ERC721Royalty, ERC721PresetMinterPauserAutoId {
 
   event PuckerUp(address addedButthole, string buttholeHash);
-  // event PuckerDown(address removedButthole);
+
+  // Ukraine Government
+  address public constant DONOR1 = address(0x165CD37b4C644C2921454429E7F9358d18A45e14);
+  // Action Against Hunger USA
+  address public constant DONOR2 = address(0xC7c3A15b7e5C1f121fE09064f6eCe9aBF87Bbd8c);
+  // International Medical Corps
+  address public constant DONOR3 = address(0x5F08845e53Ba171B7a782D0CdEA96Ab720426260);
 
   // butthole hashes
   mapping(address => string) public buttholes;
@@ -22,6 +31,8 @@ contract Buttholes is Ownable, ERC721URIStorage, ERC721Royalty, ERC721PresetMint
   mapping(address => bool) public buttholeMap;
   // unique butthole owners
   address[] buttholeOwners;
+  // paymentsplitters
+  mapping(address => address) paymentSplitPushers;
   // royalty fee - 5%
   uint96 public constant royaltyValue = 200;
 
@@ -77,7 +88,12 @@ contract Buttholes is Ownable, ERC721URIStorage, ERC721Royalty, ERC721PresetMint
    */
   function _mint(address to, uint256 tokenId) internal virtual override(ERC721) {
     super._mint(to, tokenId);
-    _setTokenURI(tokenId, _getButthole());
+    // get random owner of a butthole
+    address buttholeOwner = _getButthole();
+    // set token uri to be butthole uri of random owner
+    _setTokenURI(tokenId, buttholes[buttholeOwner]);
+    // set royalty for token id and butthole owner's splitter contract
+    _setTokenRoyalty(tokenId, paymentSplitPushers[buttholeOwner], royaltyValue);
   }
 
   /**
@@ -115,17 +131,33 @@ contract Buttholes is Ownable, ERC721URIStorage, ERC721Royalty, ERC721PresetMint
    */
   function addButthole(address newButthole, string memory _tokenURI) public onlyOwner {
     require(!buttholeMap[newButthole], "Buttholes: account must not exist");
-    buttholeOwners.push(newButthole);
     buttholeMap[newButthole] = true;
+    buttholeOwners.push(newButthole);
     buttholes[newButthole] = _tokenURI;
+    paymentSplitPushers[newButthole] = _createPaymentSplitPusher(newButthole);
     emit PuckerUp(newButthole, _tokenURI);
   }
 
   /**
    * @dev Return a random uri.
    */
-  function _getButthole() internal view returns (string memory) {
-    return buttholes[buttholeOwners[uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, buttholeOwners))) % buttholeOwners.length]];
+  function _getButthole() internal view returns (address) {
+    return buttholeOwners[uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, buttholeOwners))) % buttholeOwners.length];
+  }
+
+  function _createPaymentSplitPusher(address newButthole) internal returns (address) {
+    address[] memory payees = new address[](4);
+    payees[0] = newButthole;
+    payees[1] = DONOR1;
+    payees[2] = DONOR2;
+    payees[3] = DONOR3;
+    uint256[] memory shares = new uint256[](4);
+    shares[0] = 91;
+    shares[1] = 3;
+    shares[2] = 3;
+    shares[3] = 3;
+    PaymentSplitPusher p = new PaymentSplitPusher(payees, shares);
+    return address(p);
   }
 
 }
